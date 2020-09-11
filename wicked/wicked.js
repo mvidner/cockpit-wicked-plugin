22217924c2ed1800cecaf8a95a6950e254672780
Martin Vidner <mvidner@suse.com>
2020-09-11 14:28:02 +0200

    Shortened

--- wicked/wicked.js
+++ wicked/wicked.js
@@ -10,6 +10,12 @@ function wicked_list_ifaces() {
         .catch(wicked_list_fail);
 }
 
+/* create a DOM element */
+function el(name) {
+    return document.createElement(name);
+}
+
+
 function wicked_list_success(data) {
     let xmlDoc;
 
@@ -17,34 +23,24 @@ function wicked_list_success(data) {
     let footer = "</root>\n";
     data = header + data + footer;
 
-    if (window.DOMParser) {
-        console.log("MV: domparser");
-        parser = new DOMParser();
-        xmlDoc = parser.parseFromString(data, "text/xml");
-    }
-    else // MSIE
-    {
-        console.log("MV: msie");
-        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
-        xmlDoc.async = false;
-        xmlDoc.loadXML(data); // was: txt
-    }
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(data, "text/xml");
 
     let ifaces = xmlDoc.getElementsByTagName("object");
     for (let i = 0; i < ifaces.length; i++) {
         let iface = ifaces[i];
 
-        let tr_e = document.createElement("tr");
+        let tr_e = el("tr");
         tr_e.classList.add("listing-ct-item");
 
-        let name_e = document.createElement("th");
+        let name_e = el("th");
         name_e.append(document.createTextNode(iface.getAttribute("path")));
 
-        let ip_e = document.createElement("th");
+        let ip_e = el("th");
         get_ip(iface, ip_e);
 
-        let sending_e = document.createElement("th");
-        let receiving_e = document.createElement("th");
+        let sending_e = el("th");
+        let receiving_e = el("th");
 
         tr_e.appendChild(name_e);
         tr_e.appendChild(ip_e);
@@ -61,7 +57,7 @@ function get_ip(object_e, th_e) {
     for (let i = 0; i < local_es.length; i++) {
         let local_e = local_es[i];
         let local_s = local_e.textContent;
-        let p_e = document.createElement("p");
+        let p_e = el("p");
         p_e.append(document.createTextNode(local_s));
         th_e.appendChild(p_e);
     }
